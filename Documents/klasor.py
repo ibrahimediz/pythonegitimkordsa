b1ba7f627a9bdb51a3b9d157ea227cff91e46f74
İbrahim EDİZ <edizdesign@gmail.com>
2023-03-03 13:19:54 +0000

    03032023_2

--- Documents/klasor.py
+++ Documents/klasor.py
@@ -1,11 +1,11 @@
 import os
 print(*os.listdir("/workspace/pythonegitimkordsa/Exercises"))
-filename = "01_04_tuple"
+filename = "01_05_dict"
 metin = """
 var1 = '41,5,59,N'
 """
 for item in os.listdir("/workspace/pythonegitimkordsa/Exercises"):
-    with open(f"/workspace/pythonegitimkordsa/Exercises/{item}/{filename}.py","a+") as dosya:
+    with open(f"/workspace/pythonegitimkordsa/Exercises/{item}/{filename}.ipynb","a+") as dosya:
         pass
         # dosya.seek(0)
         # dosya.write(metin)
