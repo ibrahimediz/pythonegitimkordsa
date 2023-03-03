fcf0c8f6070792304b902e49703d1b416307d964
İbrahim EDİZ <edizdesign@gmail.com>
2023-03-03 15:21:08 +0000

    03032023_1

--- Documents/klasor.py
+++ Documents/klasor.py
@@ -5,9 +5,11 @@ metin = """
 var1 = '41,5,59,N'
 """
 for item in os.listdir("/workspace/pythonegitimkordsa/Exercises"):
-    with open(f"/workspace/pythonegitimkordsa/Exercises/{item}/{filename}.ipynb","a+") as dosya:
+    with open(f"/workspace/pythonegitimkordsa/Exercises/{item}/{filename}.py","a+") as dosya:
         pass
         # dosya.seek(0)
         # dosya.write(metin)
 
     
+import json
+json.dumps()
